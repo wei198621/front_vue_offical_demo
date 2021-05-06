938c6c8c2198b0fe350cdf82ef32ddb6ba7ad799
wei198621 <wei198621@126.com>
2021-05-06 22:40:16 +0800

    add

---
+++ uniapptest/components/mycomponent01.vue
@@ -0,0 +1,39 @@
+<template>
+	<view>
+		<h1 v-on:click="click_inner01" >{{title}}</h1>
+		<div>{{content}}</div>
+	</view>
+</template>
+
+<script>
+	
+	export default {
+		data() {
+			return {				
+			};
+		},onLoad() {			
+		},
+		props:{
+			title:{
+				type: String,
+				default:"Title01",
+			},
+			content:{
+				type: String,
+				default:"Content001",
+			},
+		},
+		methods:{	
+			click_inner01: function(){
+				console.log( this.title);
+				this.$emit("click_out",this.title);
+			}
+		},
+		components:{
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ uniapptest/pages/index/index.vue
@@ -0,0 +1,65 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		
+		<switch checked="true"></switch>
+		<mycomponent01 v-on:click_out="click_out01"></mycomponent01>
+		
+		
+		 
+	</view>
+</template>
+
+<script>
+	import mycomponent01 from "@/components/mycomponent01.vue"
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			click_out01: function(){
+				console.log("click in index.vue ");
+			}
+
+		},
+		components:{
+			mycomponent01,
+		}
+	}
+</script>
+
+<style>
+	.content {
+        display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
